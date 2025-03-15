3390c64016ab72e138d10220632f9219d8e128a1
Yuri <yuri-chrystian@outlook.com>
2025-03-14 23:03:09 -0300

    Começando o projeto

---
+++ src/main/java/br/edu/unifaj/mobile/fajbank/FajbankApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unifaj.mobile.fajbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FajbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FajbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/unifaj/mobile/fajbank/FajbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unifaj.mobile.fajbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FajbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
